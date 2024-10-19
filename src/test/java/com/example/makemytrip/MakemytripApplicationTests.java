8d01b6fa76427624acc4f7dbe7b90bb5f00c4db6
vikasjoshi169 <vikasjoshi169@gmail.com>
2024-10-19 14:02:49 +0530

    initial commit

---
+++ src/test/java/com/example/makemytrip/MakemytripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makemytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakemytripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
